4a2a18ef05a7d8fd5f3b60a8b5a60f20942c20a6
Rayssa <rayssa.manoella@escolar.ifrn.edu.br>
2024-11-29 15:45:56 -0300

    mudando as cores do grafico

--- scripts/main.js
+++ scripts/main.js
@@ -36,92 +36,60 @@ function openModal(date){
 }
 
 //função load() será chamada quando a pagina carregar:
-
 function load (){ 
   const date = new Date() 
-  
-
   //mudar titulo do mês:
   if(nav !== 0){
     date.setMonth(new Date().getMonth() + nav) 
-  }
-  
+  } 
   const day = date.getDate()
   const month = date.getMonth()
-  const year = date.getFullYear()
-
-  
-  
+  const year = date.getFullYear()  
   const daysMonth = new Date (year, month + 1 , 0).getDate()
   const firstDayMonth = new Date (year, month, 1)
-  
-
   const dateString = firstDayMonth.toLocaleDateString('pt-br', {
     weekday: 'long',
     year:    'numeric',
     month:   'numeric',
     day:     'numeric',
   })
-  
-
   const paddinDays = weekdays.indexOf(dateString.split(', ') [0])
-  
   //mostrar mês e ano:
-  document.getElementById('monthDisplay').innerText = `${date.toLocaleDateString('pt-br',{month: 'long'})}, ${year}`
-
-  
+  document.getElementById('monthDisplay').innerText = `${date.toLocaleDateString('pt-br',{month: 'long'})}, ${year}` 
   calendar.innerHTML =''
-
   // criando uma div com os dias:
-
   for (let i = 1; i <= paddinDays + daysMonth; i++) {
     const dayS = document.createElement('div')
     dayS.classList.add('day')
-
     const dayString = `${month + 1}/${i - paddinDays}/${year}`
-
-    //condicional para criar os dias de um mês:
-     
+    //condicional para criar os dias de um mês:    
     if (i > paddinDays) {
       dayS.innerText = i - paddinDays
-      
-
-      const eventDay = events.find(event=>event.date === dayString)
-      
+      const eventDay = events.find(event=>event.date === dayString)     
       if(i - paddinDays === day && nav === 0){
         dayS.id = 'currentDay'
       }
-
-
       if(eventDay){
         const eventDiv = document.createElement('div')
         eventDiv.classList.add('event')
         eventDiv.innerText = eventDay.title
         dayS.appendChild(eventDiv)
-
       }
-
       dayS.addEventListener('click', ()=> openModal(dayString))
-
     } else {
       dayS.classList.add('padding')
-    }
-
-    
+    }    
     calendar.appendChild(dayS)
   }
 }
-
 function closeModal(){
   eventTitleInput.classList.remove('error')
   newEvent.style.display = 'none'
   backDrop.style.display = 'none'
   deleteEventModal.style.display = 'none'
-
   eventTitleInput.value = ''
   clicked = null
   load()
-
 }
 function saveEvent(){
   if(eventTitleInput.value){
@@ -139,37 +107,25 @@ function saveEvent(){
     eventTitleInput.classList.add('error')
   }
 }
-
 function deleteEvent(){
-
   events = events.filter(event => event.date !== clicked)
   localStorage.setItem('events', JSON.stringify(events))
   closeModal()
 }
-
 // botões 
-
 function buttons (){
   document.getElementById('backButton').addEventListener('click', ()=>{
     nav--
-    load()
-    
+    load()  
   })
-
   document.getElementById('nextButton').addEventListener('click',()=>{
     nav++
-    load()
-    
+    load()  
   })
-
   document.getElementById('saveButton').addEventListener('click',()=> saveEvent())
-
   document.getElementById('cancelButton').addEventListener('click',()=>closeModal())
-
   document.getElementById('deleteButton').addEventListener('click', ()=>deleteEvent())
-
   document.getElementById('closeButton').addEventListener('click', ()=>closeModal())
-  
 }
 buttons()
 load()
